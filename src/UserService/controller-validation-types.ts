37896a47a731f0c079fdc5be3b422e4acaeba810
ericellb <ericellb@gmail.com>
2020-01-27 23:41:34 -0500

    Validaiton for student create route

--- src/UserService/controller-validation-types.ts
+++ src/UserService/controller-validation-types.ts
@@ -2,17 +2,27 @@ import Joi from '@hapi/joi';
 
 export const CreateUserDTO = Joi.object({
   schoolId: Joi.number().required(),
+  username: Joi.string().required(),
+  password: Joi.string().required(),
   firstName: Joi.string().required(),
   lastName: Joi.string().required(),
   address: Joi.string().required(),
+  city: Joi.string().required(),
+  country: Joi.string().required(),
+  postal_code: Joi.string().required(),
   dob: Joi.date().required(),
 });
 
 export interface CreateStudentDTO {
   schoolId: number;
+  username: string;
+  password: string;
   firstName: string;
   lastName: string;
   address: string;
+  city: string;
+  country: string;
+  postal_code: string;
   dob: Date;
   type: 'Student';
 }
